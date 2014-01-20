6977a8e410be5e0c2b44d5708e52bc6d431ba7ee
Paul Prince <paul@littlebluetech.com>
2014-01-20 02:12:55 -0500

    Bug fixes.

--- virtualenvwrapper/npm.py
+++ virtualenvwrapper/npm.py
@@ -7,8 +7,8 @@ log = logging.getLogger(__name__)
 
 def post_activate_source(args):
     return """
-if [ -e "${VIRTUAL_ENV}/.npm"; then
-    if [ -n "${NPM_CONFIG_PREFIX+1}" ]; then
+if [ -e "${VIRTUAL_ENV}/.npm" ]; then
+    if [ -n "${NPM_CONFIG_PREFIX+x}" ]; then
         export _OLD_NPM_CONFIG_PREFIX="$NPM_CONFIG_PREFIX"
     fi
     export NPM_CONFIG_PREFIX=$VIRTUAL_ENV
@@ -20,8 +20,8 @@ fi
 
 def pre_deactivate_source(args):
     return  """
-if [ -e "${VIRTUAL_ENV}/.npm"; then
-    if [ -n "${_OLD_NPM_CONFIG_PREFIX+1}" ]; then
+if [ -e "${VIRTUAL_ENV}/.npm" ]; then
+    if [ -n "${_OLD_NPM_CONFIG_PREFIX+x}" ]; then
         NPM_CONFIG_PREFIX="$_OLD_NPM_CONFIG_PREFIX"
         export NPM_CONFIG_PREFIX
         unset _OLD_NPM_CONFIG_PREFIX
