4d36c7fc3745416df5d1b84fea0f2beda45429b6
Paul Prince <paul@littlebluetech.com>
2014-01-19 22:23:34 -0500

    Add check for .npm inside venv.

--- virtualenvwrapper/npm.py
+++ virtualenvwrapper/npm.py
@@ -7,11 +7,12 @@ log = logging.getLogger(__name__)
 
 def post_activate_source(args):
     return """
-#patch to wrap npm inside the virtual env
-if [ -n "${NPM_CONFIG_PREFIX+1}" ]; then
-    export _OLD_NPM_CONFIG_PREFIX="$NPM_CONFIG_PREFIX"
+if [ -e "${VIRTUAL_ENV}/.npm"; then
+    if [ -n "${NPM_CONFIG_PREFIX+1}" ]; then
+        export _OLD_NPM_CONFIG_PREFIX="$NPM_CONFIG_PREFIX"
+    fi
+    export NPM_CONFIG_PREFIX=$VIRTUAL_ENV
 fi
-export NPM_CONFIG_PREFIX=$VIRTUAL_ENV
 
 """
 
@@ -19,13 +20,14 @@ export NPM_CONFIG_PREFIX=$VIRTUAL_ENV
 
 def pre_deactivate_source(args):
     return  """
-#restore the value before exiting the venv
-if [ -n "${_OLD_NPM_CONFIG_PREFIX+1}" ]; then
-    NPM_CONFIG_PREFIX="$_OLD_NPM_CONFIG_PREFIX"
-    export NPM_CONFIG_PREFIX
-    unset _OLD_NPM_CONFIG_PREFIX
-else
-    unset NPM_CONFIG_PREFIX
+if [ -e "${VIRTUAL_ENV}/.npm"; then
+    if [ -n "${_OLD_NPM_CONFIG_PREFIX+1}" ]; then
+        NPM_CONFIG_PREFIX="$_OLD_NPM_CONFIG_PREFIX"
+        export NPM_CONFIG_PREFIX
+        unset _OLD_NPM_CONFIG_PREFIX
+    else
+        unset NPM_CONFIG_PREFIX
+    fi
 fi
 
 """
